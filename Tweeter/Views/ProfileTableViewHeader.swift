4f6ba11c4b375c42fe94ef8f9c7dceb6979177da
NilayYadav <nilayyadav10@gmail.com>
2023-08-04 21:40:46 +0530

    ProfileViewController

---
+++ Tweeter/Views/ProfileTableViewHeader.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileHeader.swift
+//  Tweeter
+//
+//  Created by Nilay on 04/08/23.
+//
+
+import UIKit
+
+class ProfileHeader: UIView {
+ 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .blue
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
